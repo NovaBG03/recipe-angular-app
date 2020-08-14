a817f45b5a9b060c990b95314018efacbaa455bc
Nikita Koynov <nikicha03@gmail.com>
2020-08-14 23:18:34 +0300

    recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from "../recipe.model";
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Potatoes", "Delicious potato dish", "https://www.tasteofhome.com/wp-content/uploads/2018/01/exps15817_PR15D03_03_1b-696x696.jpg"),
+    new Recipe("Ice Cream Cake", "Copycat dairy queen ice cream cake", "https://www.browneyedbaker.com/wp-content/uploads/2015/06/ice-cream-cake-24-600.jpg")
+  ];
 
   constructor() { }
 
