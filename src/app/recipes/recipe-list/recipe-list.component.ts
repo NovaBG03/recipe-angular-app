d87d4a2a004a401c3322f1285bfe8aec170f612f
Nikita Koynov <nikicha03@gmail.com>
2020-08-16 14:40:08 +0300

    showing selected reicpe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from "../recipe.model";
 
@@ -8,6 +8,7 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() showRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe("Potatoes", "Delicious potato dish", "https://www.tasteofhome.com/wp-content/uploads/2018/01/exps15817_PR15D03_03_1b-696x696.jpg"),
     new Recipe("Ice Cream Cake", "Copycat dairy queen ice cream cake", "https://www.browneyedbaker.com/wp-content/uploads/2015/06/ice-cream-cake-24-600.jpg")
@@ -18,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(selectedRecipe: Recipe) {
+    this.showRecipe.emit(selectedRecipe);
+  }
 }
