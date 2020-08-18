5e5f0d6fb03d373e90836786ad287e38822a5fd4
Nikita Koynov <nikicha03@gmail.com>
2020-08-18 18:13:45 +0300

    added ingredients to recipe.model

--- src/app/recipes/recipe..service.ts
+++ src/app/recipes/recipe..service.ts
@@ -1,10 +1,28 @@
-import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+
+import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe("Potatoes", "Delicious potato dish", "https://www.tasteofhome.com/wp-content/uploads/2018/01/exps15817_PR15D03_03_1b-696x696.jpg"),
-    new Recipe("Ice Cream Cake", "Copycat dairy queen ice cream cake", "https://www.browneyedbaker.com/wp-content/uploads/2015/06/ice-cream-cake-24-600.jpg")
+    new Recipe(
+      'Potatoes',
+      'Delicious potato dish',
+      'https://www.tasteofhome.com/wp-content/uploads/2018/01/exps15817_PR15D03_03_1b-696x696.jpg',
+      [
+        new Ingredient('Potato', 3),
+        new Ingredient('Butter', 1)
+      ]
+    ),
+    new Recipe(
+      'Ice Cream Cake',
+      'Copycat dairy queen ice cream cake',
+      'https://www.browneyedbaker.com/wp-content/uploads/2015/06/ice-cream-cake-24-600.jpg',
+      [
+        new Ingredient('Ice Cream', 2),
+        new Ingredient('Cake', 1)
+      ]
+    )
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
