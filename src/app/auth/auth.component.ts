4bab5c38ab643aeec87a12c777bbdac2ef3cd8e0
Nikita Koynov <nikicha03@gmail.com>
2020-09-01 19:06:20 +0300

    handling form input

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
@@ -17,4 +18,9 @@ export class AuthComponent implements OnInit {
     this.isLoginMode = !this.isLoginMode;
   }
 
+  onSubmit(authForm: NgForm) {
+    console.log(authForm.value);
+
+    authForm.reset();
+  }
 }
