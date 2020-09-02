b8ee7b7070a8960850ee0d4bcc72ea91d9cd0bd1
Nikita Koynov <nikicha03@gmail.com>
2020-09-02 13:32:21 +0300

    sending singup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from "@angular/forms";
 
+import {AuthService} from "./auth.service";
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -9,7 +11,7 @@ import {NgForm} from "@angular/forms";
 export class AuthComponent implements OnInit {
   isLoginMode = false;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +21,23 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(authForm: NgForm) {
-    console.log(authForm.value);
+    if (authForm.invalid) {
+      return;
+    }
+
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+
+    if (this.isLoginMode) {
+      // ...
+    } else {
+      this.authService.singUp(email, password)
+        .subscribe(data => {
+          console.log(data);
+        }, error => {
+          console.log(error);
+        })
+    }
 
     authForm.reset();
   }
