59ebbb2de1af12a7016afe4a0fad011f7d33e7e5
Nikita Koynov <nikicha03@gmail.com>
2020-10-15 22:48:58 +0300

    core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
-import {DropdownDirective} from "./shared/dropdown.directive";
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {RecipeService} from "./recipes/recipe.service";
 import {AuthComponent} from './auth/auth.component';
-import {SpinnerComponent} from "./shared/spinner/spinner.component";
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import {AlertComponent} from './shared/alert/alert.component';
-import {PlaceholderDirective} from './shared/placeholder/placeholder.directive';
 import {RecipesModule} from "./recipes/recipes.module";
 import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core.module";
 
 
 @NgModule({
@@ -31,16 +25,9 @@ import {SharedModule} from "./shared/shared.module";
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
