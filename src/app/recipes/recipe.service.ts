fcc19946f5ddbb1594ba5a490d02292f7f28f94a
Nikita Koynov <nikicha03@gmail.com>
2020-10-24 18:59:22 +0300

    add ingredients action

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,38 +1,20 @@
 import {Injectable} from "@angular/core";
+import {Subject} from "rxjs";
+import {Store} from "@ngrx/store";
+
 
 import {Recipe} from "./recipe.model";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
-import {Subject} from "rxjs";
+import * as ShoppingListActions from "../shopping-list/store/shopping-list.actions";
 
 @Injectable()
 export class RecipeService {
-  // private recipes: Recipe[] = [
-  //   new Recipe(
-  //     'Potatoes',
-  //     'Delicious potato dish',
-  //     'https://www.tasteofhome.com/wp-content/uploads/2018/01/exps15817_PR15D03_03_1b-696x696.jpg',
-  //     [
-  //       new Ingredient('Potato', 3),
-  //       new Ingredient('Butter', 1)
-  //     ]
-  //   ),
-  //   new Recipe(
-  //     'Ice Cream Cake',
-  //     'Copycat dairy queen ice cream cake',
-  //     'https://www.browneyedbaker.com/wp-content/uploads/2015/06/ice-cream-cake-24-600.jpg',
-  //     [
-  //       new Ingredient('Ice Cream', 2),
-  //       new Ingredient('Cake', 1)
-  //     ]
-  //   )
-  // ];
-
+  recipesChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [];
 
-  recipesChanged = new Subject<Recipe[]>();
-
-  constructor(private shoppingListService: ShoppingListService) {
+  constructor(private shoppingListService: ShoppingListService,
+              private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) {
   }
 
   setRecipes(recipes: Recipe[]) {
@@ -49,7 +31,7 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.shoppingListService.addIngredients(ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
   }
 
   updateRecipe(id: number, recipe: Recipe) {
