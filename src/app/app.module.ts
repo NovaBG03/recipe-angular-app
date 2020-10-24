8c1ac9f87ba071f7bbffc4436c537b0e1c86af88
Nikita Koynov <nikicha03@gmail.com>
2020-10-24 18:06:08 +0300

    shopping list actions and store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
+import {StoreModule} from "@ngrx/store";
 
 import {AppRoutingModule} from "./app-routing.module";
 import {SharedModule} from "./shared/shared.module";
@@ -8,6 +9,7 @@ import {CoreModule} from "./core.module";
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
+import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducer";
 
 
 @NgModule({
@@ -19,6 +21,7 @@ import {HeaderComponent} from "./header/header.component";
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     SharedModule,
     CoreModule,
   ],
