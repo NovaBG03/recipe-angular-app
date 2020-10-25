60ceae790e0dd797e4666c95472972a3d880ad6f
Nikita Koynov <nikicha03@gmail.com>
2020-10-25 20:37:20 +0200

    remove shopping list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,7 +3,6 @@ import {Observable} from "rxjs";
 import {Store} from "@ngrx/store";
 
 import {Ingredient} from '../shared/ingredient.model';
-import {ShoppingListService} from "./shopping-list.service";
 import * as fromShoppingList from "./store/shopping-list.reducer";
 import * as ShoppingListActions from "./store/shopping-list.actions"
 
@@ -15,8 +14,7 @@ import * as ShoppingListActions from "./store/shopping-list.actions"
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Observable<{ ingredients: Ingredient[] }>;
 
-  constructor(private shoppingListService: ShoppingListService,
-              private store: Store<fromShoppingList.AppState>) {
+  constructor(private store: Store<fromShoppingList.AppState>) {
   }
 
   ngOnInit(): void {
